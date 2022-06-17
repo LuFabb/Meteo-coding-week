204ae732a07503eec9ceb4ea06ab18bc889f80cb
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-17 18:28:56 -0500

    start jslogic retrieve the elements from the page

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// retrieve the elements we need from the page
+const weatherIcon = document.querySelector('.weather-icon');
+const weatherLocation = document.querySelector('.weather-location');
+const weatherTemperature = document.querySelector('.weather-temperature');
+const suggestionParagraph = document.querySelector('.suggestion');
+
+// retrieve the root element - <html> tag
+const rootElement = document.documentElement;
 
